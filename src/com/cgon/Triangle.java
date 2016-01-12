2ab7ae17939243673df5ec76bfe7c5c044780a41
sahnoune <khalil.sahnoune@berger-levrault.fr>
2016-01-12 10:02:14 +0100

    version khalil  création triangle

--- src/com/cgon/Triangle.java
+++ src/com/cgon/Triangle.java
@@ -1,5 +1,44 @@
 package com.cgon;
 
 public class Triangle {
+	private double coteA;
+    private double coteB;
+    private double coteC;
+    
+    public Triangle(){
+          this(0.0,0.0,0.0);
+    
+    }
+    
+    public Triangle(double coteA, double coteB, double coteC){
+          this.coteA = coteA;
+          this.coteB = coteB;
+          this.coteC = coteC;
+    }
+    
+    double getCoteA() {
+          return coteA;
+    }
+
+    void setCoteA(double coteA) {
+          this.setCoteA(coteA);
+    }
+
+    double getCoteB() {
+          return coteB;
+    }
+
+    void setCoteB(double coteB) {
+          this.coteB = coteB;
+    }
+
+    double getCoteC() {
+          return coteC;
+    }
+
+    void setCoteC(double coteC) {
+          this.coteC = coteC;
+    }
+
 
 }
